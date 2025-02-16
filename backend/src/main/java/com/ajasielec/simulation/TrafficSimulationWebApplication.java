c938b0404252863d4bf7a6c10f28e63eaf7ffcdb
Ania <ajasielec@student.agh.edu.pl>
2025-02-16 11:58:34 +0100

    Project init

---
+++ backend/src/main/java/com/ajasielec/simulation/TrafficSimulationWebApplication.java
@@ -0,0 +1,13 @@
+package com.ajasielec.simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficSimulationWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficSimulationWebApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ajasielec/simulation/TrafficSimulationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajasielec.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficSimulationWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
